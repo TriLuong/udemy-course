acdb694a27e474afab4e86d8cf3eeb0fe3eb3fd2
Tri Luong <triluongdl@gmail.com>
2019-11-18 14:25:03 +0700

    Add cors.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const { userRouter } = require("./routers");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 8080;
 //     next();
 //   }
 // });
-
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 
